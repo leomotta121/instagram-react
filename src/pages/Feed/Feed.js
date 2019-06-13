17c205518e253e41c8a8f769842ae747b54d60b3
Leonardo Motta <leo@domboscoatacado.com.br>
2019-06-13 19:17:49 -0300

    Prototype of Feed

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 
+import { PostSection } from './style';
+
+import more from '../../assets/more.svg';
+import like from '../../assets/like.svg';
+import comment from '../../assets/comment.svg';
+import send from '../../assets/send.svg';
+
 class Feed extends Component {
   render() {
-    return <h1>Feed</h1>;
+    return (
+      <PostSection>
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Leonardo Motta</span>
+              <span className="place">Brasilia</span>
+            </div>
+
+            <img src={more} alt="mais" />
+          </header>
+
+          <img className="img-post" src="" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+
+            <strong>999 Curtidas</strong>
+
+            <p>
+              Um post muito massa
+              <span>#react #top</span>
+            </p>
+          </footer>
+        </article>
+      </PostSection>
+    );
   }
 }
 
---
+++ src/pages/Feed/style.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const PostSection = styled.section`
+  width: 100%;
+  max-width: 580px;
+  margin: 0 auto;
+  padding: 0 30px;
+
+  article {
+    background: #fff;
+    border: 1px solid #ddd;
+    margin-top: 30px;
+
+    header {
+      padding: 20px;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .img-post {
+      width: 100%;
+    }
+
+    footer {
+      padding: 20px;
+
+      .actions {
+        margin-bottom: 10px;
+        img {
+          height: 20px;
+          margin-right: 10px;
+        }
+      }
+
+      p {
+        font-size: 13px;
+        margin-top: 2px;
+        line-height: 18px;
+
+        span {
+          color: #7159c1;
+          display: block;
+        }
+      }
+    }
+  }
+
+  .user-info {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      font-size: 13px;
+    }
+
+    .place {
+      font-size: 11px;
+      color: #666;
+      margin-top: 6px;
+    }
+  }
+`;

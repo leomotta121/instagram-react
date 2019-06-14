33a11f77e4106619dd79be65c0eab204819f87ef
Leonardo Motta <leo@domboscoatacado.com.br>
2019-06-13 22:53:15 -0300

    Add real time

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import io from 'socket.io-client';
 
 import { PostSection } from './style';
 
@@ -14,10 +15,26 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
+
     const response = await api.get('/v1/post/index');
 
     this.setState({ feed: response.data });
   }
+
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post => (post._id === likedPost._id ? likedPost : post))
+      });
+    });
+  };
 
   handleLike = id => {
     api.post(`/v1/post/like/${id}`);
